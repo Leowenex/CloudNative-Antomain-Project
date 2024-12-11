07d406e5e0b2a87bd7cc012a73d8f7074106d2b1
AntoineC <antoinecraipeau@outlook.fr>
2024-12-11 14:58:48 +0100

    Initial Commit

---
+++ message-service/src/test/java/org/antomain/message_service/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.antomain.message_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
