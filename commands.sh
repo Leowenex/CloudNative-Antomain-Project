2b47a7842f88d088846482ee2d98958a0b69df16
AntoineC <antoine.craipeau@efrei.net>
2025-01-28 12:18:46 +0100

    Utility commands, circuit breaker and virtual service update for user service.

--- commands.sh
+++ commands.sh
@@ -1,17 +1,41 @@
+# Consulter ce README
+# https://github.com/charroux/lsi1?tab=readme-ov-file
+
 # Start a sufficent Minikube cluster
 
 minikube start --memory=8192mb --cpus=4
 
-# Needed plugins
+# Install Istio
+# If the cli is not installed, follow https://istio.io/latest/docs/setup/getting-started/
 
-minikube addons enable ingress
-minikube addons enable istio-provisioner
-minikube addons enable istio
+istioctl install --set profile=demo -y
+
+kubectl label namespace default istio-injection=enabled
+
+# In istio repo root (downloaded separately) :
+
+kubectl apply -f samples/addons
 
 # Deploy helm chart
 
 helm upgrade --install cnap antomain-helm
 
-# Forwarding Istio Gateway to the service
+# Forwarding Istio Gateway
 
 kubectl -n istio-system port-forward deployment/istio-ingressgateway 31380:8080
+
+# Forwarding Kiali
+
+kubectl -n istio-system port-forward deployment/kiali 20001:20001
+
+# Forwarding Grafana
+
+kubectl -n istio-system port-forward deployment/grafana 3000:3000
+
+# Uninstall helm chart
+
+helm uninstall cnap
+
+# Remove everything
+
+kubectl delete all --all -n istio-system
